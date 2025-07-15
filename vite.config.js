7f8d15ce596648d4a870daafbb1bafb892c54fcf
Jonathan Geisler <jdgeisler@gmail.com>
2025-07-14 20:44:27 -0400

    Updated portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  build: {
+    target: 'esnext'
+  },
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  }
 });
